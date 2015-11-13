d261acd661a64a76bcc8d099c1a04071f952a029
Diogo Resende <dresende@thinkdigital.pt>
2015-11-13 11:04:09 +0000

    Adds readFileRecord

---
+++ test/integration/read-file-record.js
@@ -0,0 +1,36 @@
+var assert = require("assert");
+var Help   = require("../help");
+
+describe("Read File Record", function () {
+	it("should be [{ file, start, end }, ..] => [ data, .. ]", function () {
+		for (var i = 0; i < Help.trials; i++) {
+			var req = [];
+			var res = [];
+
+			for (var j = 0; j < 10; j++) {
+				var start = Math.round(Math.random() * 10);
+
+				req.push({
+					file  : Math.round(Math.random() * 10),
+					start : start,
+					end   : start + Math.round(Math.random() * 10)
+				});
+
+				res.push(Help.randomBlockList(5, 2));
+			}
+
+			assert.deepEqual(
+				req,
+				Help.modbus.readFileRecord.request.parse(
+					Help.modbus.readFileRecord.request.build(req).slice(1)
+				)
+			);
+			assert.deepEqual(
+				res,
+				Help.modbus.readFileRecord.response.parse(
+					Help.modbus.readFileRecord.response.build(res).slice(1)
+				)
+			);
+		}
+	});
+});
