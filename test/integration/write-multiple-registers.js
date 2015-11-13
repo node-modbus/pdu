25574cc20818944df6ee82e013402a6b4bfb3726
Diogo Resende <dresende@thinkdigital.pt>
2015-11-13 02:32:00 +0000

    Adds writeMultipleRegisters

---
+++ test/integration/write-multiple-registers.js
@@ -0,0 +1,18 @@
+var assert = require("assert");
+var Help   = require("../help");
+
+describe("Write Multiple Registers", function () {
+	it("should be [ start, end, blocks ] => [ start, end ]", function () {
+		for (var i = 0; i < Help.trials; i++) {
+			var start  = Math.round(Math.random() * 100);
+			var end    = start + 10 - 1;
+			var blocks = Help.randomBlockList(5, 2);
+			var buf1   = Help.modbus.writeMultipleRegisters.request(start, end, blocks);
+			var buf2   = Help.startEndValues(Help.modbus.writeMultipleRegisters.code, start, end, blocks);
+			var buf3   = Help.modbus.writeMultipleRegisters.response(start, end);
+
+			assert.deepEqual(buf1, buf2);
+			assert.deepEqual({ start : start, end : end }, Help.modbus.writeMultipleRegisters.parse(buf3.slice(1)));
+		}
+	});
+});
