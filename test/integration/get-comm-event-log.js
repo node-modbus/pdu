4c51165fc69571604dc2e163d6d5743181101223
Diogo Resende <dresende@thinkdigital.pt>
2017-03-30 15:56:48 +0100

    adds support for get comm event log

---
+++ test/integration/get-comm-event-log.js
@@ -0,0 +1,26 @@
+var assert = require("assert");
+var Help   = require("../help");
+
+describe("Get Comm Event Log", function () {
+	it("should be [] => [ status, event_count, message_count, events ]", function () {
+		for (var i = 0; i < Help.trials; i++) {
+			var status        = Math.round(Math.random() * 100);
+			var event_count   = Math.round(Math.random() * 100);
+			var message_count = Math.round(Math.random() * 100);
+			var events        = Help.randomBlock(Math.random() * 4);
+
+			assert.deepEqual(
+				{},
+				Help.modbus.GetCommEventLog.Request.parse(
+					Help.modbus.GetCommEventLog.Request.build()
+				)
+			);
+			assert.deepEqual(
+				{ status, event_count, message_count, events },
+				Help.modbus.GetCommEventLog.Response.parse(
+					Help.modbus.GetCommEventLog.Response.build(status, event_count, message_count, events)
+				)
+			);
+		}
+	});
+});
