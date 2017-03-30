dc59ee9a1ba81a9b61aaa67912fe3743a9de663b
Diogo Resende <dresende@thinkdigital.pt>
2017-03-30 15:45:23 +0100

    adds support for get comm event counter

---
+++ test/integration/get-comm-event-counter.js
@@ -0,0 +1,24 @@
+var assert = require("assert");
+var Help   = require("../help");
+
+describe("Get Comm Event Counter", function () {
+	it("should be [] => [ status, event_count ]", function () {
+		for (var i = 0; i < Help.trials; i++) {
+			var status      = Math.round(Math.random() * 100);
+			var event_count = Math.round(Math.random() * 100);
+
+			assert.deepEqual(
+				{},
+				Help.modbus.GetCommEventCounter.Request.parse(
+					Help.modbus.GetCommEventCounter.Request.build()
+				)
+			);
+			assert.deepEqual(
+				{ status, event_count },
+				Help.modbus.GetCommEventCounter.Response.parse(
+					Help.modbus.GetCommEventCounter.Response.build(status, event_count)
+				)
+			);
+		}
+	});
+});
