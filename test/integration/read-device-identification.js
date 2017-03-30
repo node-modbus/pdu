261b687c5408eb4427e44af3e479b54b02b5baee
Diogo Resende <dresende@thinkdigital.pt>
2017-03-30 14:50:13 +0100

    read device identification - converts types and ids to strings when possible

--- test/integration/read-device-identification.js
+++ test/integration/read-device-identification.js
@@ -2,27 +2,26 @@ var assert = require("assert");
 var Help   = require("../help");
 
 describe("Parse Device Identification", function () {
-	it("should be [ code, id ] => [ code, conformity, more, next, object, objext, .. ]", function () {
+	it("should be [ type, id ] => [ type, conformity, more, next, object, objext, .. ]", function () {
 		for (var i = 0; i < Help.trials; i++) {
-			var code       = 0x01;
-			var id         = 0x02;
-			var conformity = 0x02;
+			var type       = "BasicDeviceIdentification";
+			var id         = "MajorMinorRevision";
 			var objects    = [{
-				id: 0, value: new Buffer("ThinkDigital") // company
+				id: "VendorName", value: new Buffer("ThinkDigital") // company
 			}, {
-				id: 1, value: new Buffer("ModbusLib") // product
+				id: "ProductCode", value: new Buffer("modbus-pdu") // product
 			}];
 
 			assert.deepEqual(
-				{ code : code, id : id },
+				{ type : type, id : id },
 				Help.modbus.ReadDeviceIdentification.Request.parse(
-					Help.modbus.ReadDeviceIdentification.Request.build(code, id)
+					Help.modbus.ReadDeviceIdentification.Request.build(type, id)
 				)
 			);
 			assert.deepEqual(
-				{ code: code, conformity: conformity, more: true, next: 2, objects: objects },
+				{ type: type, conformity: type, more: true, next: 11, objects: objects },
 				Help.modbus.ReadDeviceIdentification.Response.parse(
-					Help.modbus.ReadDeviceIdentification.Response.build(code, conformity, true, objects)
+					Help.modbus.ReadDeviceIdentification.Response.build(type, type, true, objects)
 				)
 			);
 		}
