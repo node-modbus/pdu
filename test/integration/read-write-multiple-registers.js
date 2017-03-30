72f2ba3701595d99635c1e380a4ed31ac7a1bb4f
Diogo Resende <dresende@thinkdigital.pt>
2017-03-30 16:10:24 +0100

    adds support for read/write multiple registers

---
+++ test/integration/read-write-multiple-registers.js
@@ -0,0 +1,26 @@
+var assert = require("assert");
+var Help   = require("../help");
+
+describe("Read Write Multiple Registers", function () {
+	it("should be [ read_address, read_quantity, write_address, values ] => [ values ]", function () {
+		for (var i = 0; i < Help.trials; i++) {
+			var read_address  = Math.round(Math.random() * 100);
+			var read_quantity = 5;
+			var write_address = Math.round(Math.random() * 100);
+			var values        = Help.randomBlockList(read_quantity, 2);
+
+			assert.deepEqual(
+				{ read_address, read_quantity, write_address, values },
+				Help.modbus.ReadWriteMultipleRegisters.Request.parse(
+					Help.modbus.ReadWriteMultipleRegisters.Request.build(read_address, read_quantity, write_address, values)
+				)
+			);
+			assert.deepEqual(
+				{ values },
+				Help.modbus.ReadWriteMultipleRegisters.Response.parse(
+					Help.modbus.ReadWriteMultipleRegisters.Response.build(values)
+				)
+			);
+		}
+	});
+});
