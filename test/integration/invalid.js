5993962944930df55ee35da2a798b25b0ae99f98
Diogo Resende <dresende@thinkdigital.pt>
2017-06-26 16:33:42 +0100

    read-coils: fixes not properly throwing when replying to coils with null/undef

--- test/integration/invalid.js
+++ test/integration/invalid.js
@@ -1,7 +1,7 @@
 var assert = require("assert");
 var Help   = require("../help");
 
-describe("Invalid data", function () {
+describe("Invalid requests", function () {
 	it("should return null and not throw", function () {
 		assert.equal(
 			null,
@@ -11,5 +11,22 @@ describe("Invalid data", function () {
 			null,
 			Help.modbus.ReadInputRegisters.Request.parse(new Buffer([ 0x07, 0x04, 0x84 ]))
 		);
+	});
+});
+
+describe("Invalid responses", function () {
+	it("should throw", function () {
+		assert.throws(
+			function () {
+				Help.modbus.ReadCoils.Response.build();
+			},
+			/overflow/
+		);
+			assert.throws(
+				function () {
+					Help.modbus.ReadCoils.Response.build(null);
+				},
+				/overflow/
+			);
 	});
 });
